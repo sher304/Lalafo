7ab6fdb9bbe287a2eab0fcbb369c5cbd2d9117e0
sher304 <esherovshermat@gmail.com>
2022-09-11 12:26:22 +0600

    :lipstick: Done Cart View

--- Lalafo/Extension/Extension.swift
+++ Lalafo/Extension/Extension.swift
@@ -12,5 +12,7 @@ extension UIColor{
 
     static let customBG = UIColor(red: 235/255, green: 235/255, blue: 235/255, alpha: 1)
     
+    static let customDark = UIColor(red: 7/255, green: 7/255, blue: 7/255, alpha: 1)
+    
     
 }
--- Lalafo/Modules/HomeVC/HomeVC.swift
+++ Lalafo/Modules/HomeVC/HomeVC.swift
@@ -38,6 +38,7 @@ class HomeViewController: UIViewController {
         return scrollV
     }()
     
+    //MARK: Search Bar
     private lazy var searchBar: UISearchBar = {
         let searchB = UISearchBar()
         searchB.backgroundColor = .white
@@ -51,18 +52,59 @@ class HomeViewController: UIViewController {
         return searchB
     }()
     
+    private lazy var balancedCartView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .customDark
+        view.layer.cornerRadius = 14
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private lazy var balanceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Balance"
+        label.textColor = .customBG
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private lazy var visaLabel: UILabel = {
+        let label = UILabel()
+        label.text = "VISA"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private lazy var cashLabel: UILabel = {
+        let label = UILabel()
+        label.text = "$230.0"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 25, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var hashIdLabel: UILabel = {
+        let label = UILabel()
+        label.text = "****2301"
+        label.textColor = .customBG
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
     
-    //MARK: Setup Constraints
+    //MARK: Setup Views
     private func setupView(){
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
         setupConstraints()
     }
     
+    //MARK: Setup Constraints
     private func setupConstraints(){
         contentView.addSubview(searchBar)
         searchBar.snp.makeConstraints { make in
@@ -70,6 +112,37 @@ class HomeViewController: UIViewController {
             make.trailing.equalTo(-17)
             make.height.equalTo(50)
             make.top.equalTo(40)
+        }
+        
+        contentView.addSubview(balancedCartView)
+        balancedCartView.snp.makeConstraints { make in
+            make.leading.equalTo(17)
+            make.trailing.equalTo(-17)
+            make.height.equalTo(140)
+            make.top.equalTo(searchBar.snp.bottom).offset(30)
+        }
+        
+        balancedCartView.addSubview(balanceLabel)
+        balanceLabel.snp.makeConstraints { make in
+            make.leading.equalTo(18)
+            make.top.equalTo(25)
+        }
+        
+        balancedCartView.addSubview(visaLabel)
+        visaLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(-18)
+            make.centerY.equalTo(balanceLabel)
+        }
+        
+        balancedCartView.addSubview(cashLabel)
+        cashLabel.snp.makeConstraints { make in
+            make.leading.equalTo(18)
+            make.bottom.equalTo(-30)
+        }
+        balancedCartView.addSubview(hashIdLabel)
+        hashIdLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(-18)
+            make.centerY.equalTo(cashLabel)
         }
     }
 }
